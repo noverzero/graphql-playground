b8c63901afa63f3b8c7a3f8b5b8d3086cb7fbc61
dustinhuth <dustin@undefinedindustries.com>
2019-11-12 16:56:14 -0700

    adding dataLoader

--- schema.js
+++ schema.js
@@ -1,6 +1,5 @@
 const fetch = require('node-fetch')
-const util = require('util')
-const parseXML = util.promisify(require('xml2js').parseString)
+
 const {
   GraphQLSchema,
   GraphQLObjectType,
@@ -29,6 +28,10 @@ function translate(lang, str) {
     )
 }
 
+
+
+
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   description: '...',
@@ -50,6 +53,15 @@ const BookType = new GraphQLObjectType({
       resolve: xml =>
       xml.GoodreadsResponse.book[0].isbn[0]
 
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve: (xml, args, context ) => {
+        const authorElements = xml.GoodreadsResponse.book[0].authors[0].author
+        const ids = authorElements.map(elem => elem.id[0])
+        return context.authorLoader.loadMany(ids)
+
+      }
     }
   })
 })
@@ -65,14 +77,10 @@ const AuthorType = new GraphQLObjectType({
     },
     books: {
       type: new GraphQLList(BookType),
-        resolve: xml => {
+        resolve: (xml, args, context) => {
         const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem => elem.id[0]._)
         console.log('fetching books!! ')
-        return Promise.all(ids.map(id =>
-          fetch(`https://www.goodreads.com/book/show/${id}.xml?key=TUydpVGlWHngTLaoNOwBnQ`)
-            .then(response => response.text())
-            .then(parseXML)
-        ))
+        return context.bookLoader.loadMany(ids)
       }
     }
   })
@@ -89,9 +97,7 @@ module.exports = new GraphQLSchema({
         args: {
           id: { type: GraphQLInt }
         },
-        resolve: (root, args) => fetch(`https://www.goodreads.com/author/show.xml?id=${args.id}&key=TUydpVGlWHngTLaoNOwBnQ `)
-            .then(response => response.text())
-            .then(parseXML)
+        resolve: (root, args, context) => context.authorLoader.load(args.id)
       }
     })
   })
